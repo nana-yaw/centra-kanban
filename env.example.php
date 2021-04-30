6b9117307cc6d70be0cabb068e8f6cecf6c9ef43
nana-yaw <edward.osei.nyarko@gmail.com>
2021-04-30 17:13:18 +0000

    Refactor environment variables

---
+++ env.example.php
@@ -0,0 +1,8 @@
+<?php
+
+define('GH_CLIENT_ID', '');
+define('GH_CLIENT_SECRET', '');
+define('GH_ACCOUNT', '');
+define('GH_REPOSITORIES', '');
+
+?>
--- src/public/index.php
+++ src/public/index.php
@@ -1,28 +1,15 @@
 <?php
 
-use KanbanBoard\Utilities;
-
 require '../classes/KanbanBoard/Github.php';
 require '../classes/Utilities.php';
 require '../classes/KanbanBoard/Authentication.php';
 
-// $variables = [
-// 	'GH_CLIENT_ID' => 'deebdfd727072c1e2b9e',
-// 	'GH_CLIENT_SECRET' => '624c19fb7bce91d71628b061647f8b6c183646f5',
-// 	'GH_ACCOUNT' => 'nana-yaw',
-// 	'GH_REPOSITORIES' => 'imageShrink|centrakanban',
-// ];
-
-// foreach ($variables as $key => $value) {
-
-// 	putenv("$key=$value");
-// }
-
 try {
-	$repositories = explode('|', Utilities::env('GH_REPOSITORIES'));
+	require '../../vendor/autoload.php';
+	$repositories = explode('|', GH_REPOSITORIES);
 	$authentication = new \KanbanBoard\Login();
 	$token = $authentication->login();
-	$github = new GithubClient($token, Utilities::env('GH_ACCOUNT'));
+	$github = new GithubClient($token, GH_ACCOUNT);
 	$board = new \KanbanBoard\Application($github, $repositories, array('all'));
 	$data = $board->board();
 
